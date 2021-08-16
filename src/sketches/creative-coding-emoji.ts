be0808a56c370fd7e9841eaaf9fa773eccda8a8a
hyakt <hyakt0@gmail.com>
2021-08-16 00:00:00 +0000

    add: emoji

---
+++ src/sketches/creative-coding-emoji.ts
@@ -0,0 +1,35 @@
+/// <reference types="p5/global" />
+import 'p5'
+import { Graphics } from 'p5'
+
+let boxSize: number
+let pg: Graphics
+let emoji, emojiCode
+
+// @ts-expect-error
+window.setup = () => {
+  createCanvas(window.innerWidth, window.innerHeight, WEBGL)
+  boxSize = min(width, height) * 0.475
+  pg = createGraphics(400, 400)
+  pg.background(200, 200, 200)
+  pg.textAlign(CENTER, CENTER)
+}
+
+// @ts-expect-error
+window.draw = () => {
+  background(0)
+  // const emojiCode = random(127744, 128306)
+  emojiCode = random(100) < 70 ? random(127744, 128306) : random(128512, 128592)
+  emoji = String.fromCodePoint(floor(emojiCode))
+
+  pg.textSize(random(40, 100))
+  pg.text(emoji, random(width), random(height))
+
+  lights()
+  noStroke()
+  texture(pg)
+  rotateX(frameCount * 0.01)
+  rotateY(frameCount * 0.01)
+  rotateZ(frameCount * 0.01)
+  box(boxSize)
+}
