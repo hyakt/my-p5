4808b6c77d3a8759cbbe3fed15244f272f9da59c
hyakt <hyakt0@gmail.com>
2021-08-19 00:00:00 +0000

    update: toho wip

--- src/sketches/my-toho.ts
+++ src/sketches/my-toho.ts
@@ -1,49 +1,90 @@
 import 'p5'
-import { Color } from 'p5'
+import p5, { Color, Renderer, Vector } from 'p5'
 
 let circlesNum = 10
 const circles: Circle[] = []
+let baseCircle: Circle
+let cnv: Renderer
+const initialRange = 6
+const aRange = 10
+const size = 400
+const weight = 40
 
 class Circle {
   size: number
   weight: number
-  x: number
-  y: number
-  d: number
-
+  vector: Vector
+  a: number
+  tStep: number
+  seq: number
+  direction: number
   strokeColor: Color
 
-  constructor(dValue?: number, colorValue?: Color) {
-    this.size = 500
-    this.weight = 70
-    this.x = 0
-    this.y = 0
-    this.d = dValue === 0 ? dValue : random(-10, 10)
-    this.strokeColor = colorValue
-      ? colorValue
-      : color(random(100), random(100), 100)
+  constructor() {
+    this.size = size
+    this.weight = weight
+    this.a = random(-aRange, aRange)
+    this.vector = createVector(
+      random(-initialRange, initialRange),
+      random(-initialRange, initialRange),
+      0
+    )
+    this.strokeColor = color(random(100), random(100), 100, 50)
+
+    this.seq = 0
+    this.tStep = random(0, 0.1)
+    this.direction = random([0, 1])
   }
-  move() {}
+  move() {
+    this.seq += this.tStep
+  }
+  click() {}
   display() {
     push()
     stroke(this.strokeColor)
     strokeWeight(this.weight)
-    circle(width / 2 + this.d, height / 2 + this.d, this.size)
+
+    let deltaX: number
+    let deltaY: number
+    if (this.direction) {
+      deltaX = sin(this.seq) * this.a
+      deltaY = cos(this.seq) * this.a
+    } else {
+      deltaX = cos(this.seq) * this.a
+      deltaY = sin(this.seq) * this.a
+    }
+
+    circle(
+      width / 2 + deltaX + this.vector.x,
+      height / 2 + deltaY + this.vector.y,
+      this.size
+    )
     pop()
   }
 }
 
 window.setup = () => {
   colorMode(HSB, 100)
+  noFill()
   createCanvas(window.innerWidth, window.innerHeight)
   for (let i = 0; i < circlesNum; i++) {
     circles.push(new Circle())
   }
-  circles.push(new Circle(0, color(0)))
 }
 
 window.draw = () => {
+  background('#fff')
   circles.forEach((e) => {
     e.display()
+    e.move()
+  })
+  stroke(0)
+  strokeWeight(weight)
+  circle(width / 2, height / 2, size)
+}
+
+window.mouseClicked = () => {
+  circles.forEach((e) => {
+    e.click()
   })
 }
