809733935853876e4ed9ccc6d996374eb470a774
hyakt <hyakt0@gmail.com>
2021-08-29 23:17:02 +0900

    update: lena

--- src/sketches/my-lena.ts
+++ src/sketches/my-lena.ts
@@ -18,13 +18,15 @@ function setup() {
   myPhoto.loadPixels()
   myPhotoWidth = myPhoto.width
   myPhotoHeight = myPhoto.height
-  createCanvas(myPhotoWidth, myPhotoHeight)
+  createCanvas(window.innerWidth, window.innerHeight)
   smooth()
   noStroke()
-  noLoop()
+  // noLoop()
 }
 
 function draw() {
+  fill(255)
+  rect(0, 0, width, height)
   for (let x = 0; x < myPhotoWidth; x = x + 10) {
     for (let y = 0; y < myPhotoHeight; y = y + 10) {
       const pixelIndex = (x + y * myPhotoWidth) * 4
@@ -35,7 +37,19 @@ function draw() {
         myPhoto.pixels[pixelIndex + 3]
       )
       fill(c)
-      circle(x, y, 10)
+      const posX = map(x, 0, myPhotoWidth, 0, width)
+      const posY = map(y, 0, myPhotoHeight, 0, height)
+
+      let size = 10
+      if (
+        posX - size / 2 < mouseX &&
+        mouseX < posX + size / 2 &&
+        posY - size / 2 < mouseY &&
+        mouseY < posY + size / 2
+      ) {
+        size = 50
+      }
+      circle(posX, posY, size)
     }
   }
 }
